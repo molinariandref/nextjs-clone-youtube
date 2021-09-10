b2c367d2fef471f4a4f8de81af74c33b03e33d1b
André Molinari <molinari.andref@gmail.com>
2021-09-10 10:55:26 -0300

    Material-ui configuration

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+
+export default function Home() {
+  return (
+    <div>
+      Clone youtube com Next.js + Material.ui
+      <Button variant="outlined" color="secondary">Teste</Button>
+    </div>
+  );
+}
